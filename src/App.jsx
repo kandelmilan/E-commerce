5e00318901dd1a45d77384b7f7494d058037195d
kandel milan <kandelmilan@kandels-MacBook-Air.local>
2025-08-07 19:27:18 +0545

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-  import React from 'react'
-  
-  function App() {
-    return (
-      <div className='text-primary'>
-         App
-      </div>
-    )
-  }
-  
-  export default App
+import React from "react";
+import { Route, Routes } from "react-router";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>THis is home page</div>} />
+          <Route path="products" element={<div>THis is all product page</div>} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
   
