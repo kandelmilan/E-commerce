c3af8e79f00dd9b47bf9aa9232544e617668d2a4
kandelmilan <kandelr950p@gmail.com>
2025-08-24 21:03:53 +0545

    updated

---
+++ src/Layout/AdminLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminLayout() {
+    return (
+        <div>
+            hello this is admin layout page...
+        </div>
+    )
+}
+
+export default AdminLayout
